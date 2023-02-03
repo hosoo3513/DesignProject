708a914fa3ef3bb26c89ae30a7be0849def813cd
강호수 <hosoo3513@gmail.com>
2023-02-03 22:05:28 +0900

    Initial commit

---
+++ src/main/java/SogangUniv/DesignProject/DesignProjectApplication.java
@@ -0,0 +1,13 @@
+package SogangUniv.DesignProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SogangUniv/DesignProject/DesignProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package SogangUniv.DesignProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
