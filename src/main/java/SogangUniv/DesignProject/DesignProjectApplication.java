0b1a6c09dbde46150c47a838b3c00a3966651582
강호수 <hosoo3513@gmail.com>
2023-03-08 11:36:07 +0900

    fix: env 파일 적용

--- src/main/java/SogangUniv/DesignProject/DesignProjectApplication.java
+++ src/main/java/SogangUniv/DesignProject/DesignProjectApplication.java
@@ -2,8 +2,10 @@ package SogangUniv.DesignProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:env.yml")
 public class DesignProjectApplication {
 
 	public static void main(String[] args) {
